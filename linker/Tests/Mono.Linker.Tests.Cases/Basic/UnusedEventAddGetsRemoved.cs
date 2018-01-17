faaa7ae2c83a31b79f1416c39b1ebc84e059c6b5
Michael Voorhees <michaelv@unity3d.com>
2018-01-17 13:50:06 -0500

    WIP : Add test cases for event add and remove removal

---
+++ linker/Tests/Mono.Linker.Tests.Cases/Basic/UnusedEventAddGetsRemoved.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Basic {
+	class UnusedEventAddGetsRemoved {
+		public static void Main ()
+		{
+			var tmp = new Foo ();
+			tmp.Bar -= Tmp_Bar;
+		}
+
+		[Kept]
+		private static void Tmp_Bar (object sender, EventArgs e)
+		{
+		}
+
+		[KeptMember (".ctor()")]
+		public class Foo
+		{
+			[Kept]
+			[KeptBackingField]
+			[KeptEventRemoveMethod]
+			public event EventHandler<EventArgs> Bar;
+		}
+	}
+}
---
+++ linker/Tests/Mono.Linker.Tests.Cases/Basic/UnusedEventRemoveGetsRemoved.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Basic {
+	class UnusedEventRemoveGetsRemoved {
+		public static void Main ()
+		{
+			var tmp = new Foo ();
+			tmp.Bar += Tmp_Bar;
+		}
+
+		[Kept]
+		private static void Tmp_Bar (object sender, EventArgs e)
+		{
+		}
+
+		[KeptMember (".ctor()")]
+		public class Foo
+		{
+			[Kept]
+			[KeptBackingField]
+			[KeptEventAddMethod]
+			public event EventHandler<EventArgs> Bar;
+		}
+	}
+}
