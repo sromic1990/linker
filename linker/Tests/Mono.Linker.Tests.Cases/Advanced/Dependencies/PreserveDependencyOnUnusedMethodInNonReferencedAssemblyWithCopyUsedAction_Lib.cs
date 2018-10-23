4200e68d4ab6e707c5faee3e849153721af9e2d0
Michael Voorhees <michaelv@unity3d.com>
2018-10-23 12:33:05 -0400

    Delete old files that should have been removed
    Something must have gone wrong with a merge conflict or something with upstream

--- linker/Tests/Mono.Linker.Tests.Cases/Advanced/Dependencies/PreserveDependencyOnUnusedMethodInNonReferencedAssemblyWithCopyUsedAction_Lib.cs
+++
@@ -1,7 +0,0 @@
-namespace Mono.Linker.Tests.Cases.Advanced.Dependencies {
-	public class PreserveDependencyOnUnusedMethodInNonReferencedAssemblyWithCopyUsedAction_Lib {
-		public static void MethodPreservedViaDependencyAttribute ()
-		{
-		}
-	}
-}
