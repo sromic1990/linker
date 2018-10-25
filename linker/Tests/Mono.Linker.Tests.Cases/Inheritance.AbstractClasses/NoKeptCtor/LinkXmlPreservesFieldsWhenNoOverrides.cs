a13d808682f908a9983de6df5a3567714c0585b9
Michael Voorhees <michaelv@unity3d.com>
2018-10-25 13:37:09 -0400

    More tests and test clean up

--- linker/Tests/Mono.Linker.Tests.Cases/Inheritance.AbstractClasses/NoKeptCtor/LinkXmlPreservesFieldsWhenNoOverrides.cs
+++ linker/Tests/Mono.Linker.Tests.Cases/Inheritance.AbstractClasses/NoKeptCtor/LinkXmlPreservesFieldsWhenNoOverrides.cs
@@ -1,24 +1,17 @@
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
 
 namespace Mono.Linker.Tests.Cases.Inheritance.AbstractClasses.NoKeptCtor {
-	public class LinkXmlPreservesMethodsWhenNoOverrides {
+	public class LinkXmlPreservesFieldsWhenNoOverrides {
 		public static void Main ()
 		{
-			StaticMethodOnlyUsed.StaticMethod ();
 		}
 
 		abstract class Base {
 		}
 
 		[Kept]
-		class StaticMethodOnlyUsed : Base {
-			[Kept]
+		class Bar : Base {
 			public void OtherMethod ()
-			{
-			}
-
-			[Kept]
-			public static void StaticMethod ()
 			{
 			}
 		}
--- linker/Tests/Mono.Linker.Tests.Cases/Inheritance.Complex/NoKeptCtor/LinkXmlPreservesOverriddenMethodAndInterfacesStillSwept.cs
+++ linker/Tests/Mono.Linker.Tests.Cases/Inheritance.Complex/NoKeptCtor/LinkXmlPreservesOverriddenMethodAndInterfacesStillSwept.cs
@@ -1,7 +1,7 @@
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
 
-namespace Mono.Linker.Tests.Cases.Inheritance.AbstractClasses.NoKeptCtor {
-	public class LinkXmlPreservesOverriddenMethod {
+namespace Mono.Linker.Tests.Cases.Inheritance.Complex.NoKeptCtor {
+	public class LinkXmlPreservesOverriddenMethodAndInterfacesStillSwept {
 		public static void Main ()
 		{
 		}
@@ -12,13 +12,29 @@ namespace Mono.Linker.Tests.Cases.Inheritance.AbstractClasses.NoKeptCtor {
 			public abstract void Foo ();
 		}
 
+		interface IOne {
+			void One ();
+		}
+		
+		interface ITwo {
+			void Two ();
+		}
+
 		[Kept]
 		// We have to keep the base type in this case because the link xml requested that all methods be kept.
 		// Removing the base type with an override kept would produce invalid IL. 
 		[KeptBaseType (typeof (Base))]
-		class Bar : Base {
+		class Bar : Base, IOne, ITwo {
 			[Kept]
 			public override void Foo ()
+			{
+			}
+
+			public void One ()
+			{
+			}
+
+			public void Two ()
 			{
 			}
 		}
