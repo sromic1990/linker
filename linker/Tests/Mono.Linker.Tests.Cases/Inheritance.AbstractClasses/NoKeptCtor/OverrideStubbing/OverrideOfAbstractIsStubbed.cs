69c25e3def4965c26578a41d483d9b0a404d1850
Michael Voorhees <michaelv@unity3d.com>
2019-02-08 13:44:53 -0500

    Add the first test

---
+++ linker/Tests/Mono.Linker.Tests.Cases/Inheritance.AbstractClasses/NoKeptCtor/OverrideStubbing/OverrideOfAbstractIsStubbed.cs
@@ -0,0 +1,44 @@
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Inheritance.AbstractClasses.NoKeptCtor.OverrideStubbing {
+	public class OverrideOfAbstractIsStubbed {
+		public static void Main ()
+		{
+			Base b = HelperToMarkFooAndRequireBase ();
+			b.Method ();
+		}
+
+		[Kept]
+		static Foo HelperToMarkFooAndRequireBase ()
+		{
+			return null;
+		}
+
+		[Kept]
+		abstract class Base
+		{
+			[Kept]
+			public abstract void Method ();
+		}
+
+		[Kept]
+		[KeptBaseType (typeof (Base))]
+		class Foo : Base {
+			[Kept]
+			[ExpectedInstructionSequence(new []
+			{
+				"ldstr",
+				"newobj",
+				"throw"
+			})]
+			public override void Method ()
+			{
+				UsedByOverride ();
+			}
+
+			void UsedByOverride ()
+			{
+			}
+		}
+	}
+}
